6166028012cebd4543a91a1fed2ae467db4113e9
Pedro Emmanuel - Turma 27 - Tribo A <sombraios@hotmail.com>
2022-10-04 20:33:43 -0300

    resolvendo criterio 4

--- script.js
+++ script.js
@@ -9,14 +9,13 @@ coresDaPaleta2.setAttribute('class', 'color');
 coresDaPaleta3.setAttribute('class', 'color');
 coresDaPaleta4.setAttribute('class', 'color');
 coresDaPaleta1.setAttribute('id', 'black');
-coresDaPaleta2.setAttribute('id', 'cor1');
-coresDaPaleta3.setAttribute('id', 'cor2');
-coresDaPaleta4.setAttribute('id', 'cor3');
-paletaDeCores.appendChild(coresDaPaleta1); paletaDeCores.appendChild(coresDaPaleta2); paletaDeCores.appendChild(coresDaPaleta3); paletaDeCores.appendChild(coresDaPaleta4);
+paletaDeCores.appendChild(coresDaPaleta1);
+paletaDeCores.appendChild(coresDaPaleta2);
+paletaDeCores.appendChild(coresDaPaleta3);
+paletaDeCores.appendChild(coresDaPaleta4);
 
 // seção 2 - criando botão
-let arrayDeCores = ['blue', 'red', 'green', 'gray', 'yellow', 'orange', 'purple', 'pink'];
-
+const arrayDeCores = ['blue', 'red', 'green', 'gray', 'yellow', 'orange', 'purple', 'pink'];
 const corpo = document.body;
 const botaoReset = document.createElement('button');
 botaoReset.innerHTML = 'Cores aleatórias';
@@ -24,10 +23,25 @@ botaoReset.setAttribute('id', 'button-random-color');
 corpo.appendChild(botaoReset);
 
 function randomizer() {
-for (let index = 0; index <= 3; index = index + 1){
-coresDaPaleta2.style.backgroundColor = arrayDeCores[Math.floor(Math.random() * 9)]
-coresDaPaleta3.style.backgroundColor = arrayDeCores[Math.floor(Math.random() * 9)]
-coresDaPaleta4.style.backgroundColor = arrayDeCores[Math.floor(Math.random() * 9)]
-}
+  const cor1 = arrayDeCores[Math.floor(Math.random() * 9)];
+  const cor2 = arrayDeCores[Math.floor(Math.random() * 9)];
+  const cor3 = arrayDeCores[Math.floor(Math.random() * 9)];
+  coresDaPaleta2.style.backgroundColor = cor1;
+  coresDaPaleta3.style.backgroundColor = cor2;
+  coresDaPaleta4.style.backgroundColor = cor3;
+  const cores = [cor1, cor2, cor3];
+  localStorage.setItem('colorPalette', JSON.stringify(cores));
 }
 botaoReset.addEventListener('click', randomizer);
+
+
+window.onload = function () {
+    coresDaPaleta2.style.backgroundColor = 'red';
+    coresDaPaleta3.style.backgroundColor = 'blue';
+    coresDaPaleta4.style.backgroundColor = 'green';
+    const listaDeCores = document.getElementsByClassName('color');
+    const cores = JSON.parse(localStorage.getItem('colorPalette'));
+    listaDeCores[1].style.backgroundColor = cores[0]
+    listaDeCores[2].style.backgroundColor = cores[1]
+    listaDeCores[3].style.backgroundColor = cores[2]
+};
