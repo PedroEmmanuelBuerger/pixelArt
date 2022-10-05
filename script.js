da16205a857a7eaaad48c36ce293cec6fa24e443
Pedro Emmanuel - Turma 27 - Tribo A <sombraios@hotmail.com>
2022-10-05 13:01:22 -0300

    resolvendo criterio 9

--- script.js
+++ script.js
@@ -4,7 +4,6 @@ const coresDaPaleta1 = document.createElement('div');
 const coresDaPaleta2 = document.createElement('div');
 const coresDaPaleta3 = document.createElement('div');
 const coresDaPaleta4 = document.createElement('div');
-// coresDaPaleta1.setAttribute('class', 'color');
 coresDaPaleta2.setAttribute('class', 'color');
 coresDaPaleta3.setAttribute('class', 'color');
 coresDaPaleta4.setAttribute('class', 'color');
@@ -31,9 +30,11 @@ function randomizer() {
   const cor1 = arrayDeCores[Math.floor(Math.random() * 15)];
   const cor2 = arrayDeCores[Math.floor(Math.random() * 15)];
   const cor3 = arrayDeCores[Math.floor(Math.random() * 15)];
+
   coresDaPaleta2.style.backgroundColor = cor1;
   coresDaPaleta3.style.backgroundColor = cor2;
   coresDaPaleta4.style.backgroundColor = cor3;
+
   const cores = [cor1, cor2, cor3];
   localStorage.setItem('colorPalette', JSON.stringify(cores));
 }
@@ -51,30 +52,41 @@ window.onload = function () {
 };
 
 // seção 3 - criando o quadro
-function quadro(valor1){
-let quadrados = document.createElement('section')
-corpo.appendChild(quadrados);
-container = document.createElement('div')
-quadrados.appendChild(container)
-container.setAttribute('id', 'pixel-board')
+function quadro(valor1) {
+  let quadrados = document.createElement('section')
+  corpo.appendChild(quadrados);
+  container = document.createElement('div')
+  quadrados.appendChild(container)
+  container.setAttribute('id', 'pixel-board')
 
 
-if (valor1 == 5 || valor1 == undefined){
-for (let index = 0; index <5 ; index = index + 1){
-let divs = document.createElement('div')
-container.appendChild(divs)
-for( let index3 = 0; index3 < 5; index3 = index3 + 1){
-  let divs = document.createElement('div')
-  divs.setAttribute('class', 'pixel')
-  container.appendChild(divs)
-  divs.style.display = 'inline-block'
-}
-}
-}
-else for (let index2 = 0; index2 <=valor1 -1; index2 = index2 + 1){
-  let divs = document.createElement('div')
-  divs.setAttribute('class', 'pixel')
-  container.appendChild(divs)
-}
+  if (valor1 == 5 || valor1 == undefined) {
+    for (let index = 0; index < 5; index = index + 1) {
+      let divs = document.createElement('div')
+      container.appendChild(divs)
+      for (let index3 = 0; index3 < 5; index3 = index3 + 1) {
+        let divs = document.createElement('div')
+        divs.setAttribute('class', 'pixel')
+        container.appendChild(divs)
+        divs.style.display = 'inline-block'
+      }
+    }
+  }
+  else for (let index2 = 0; index2 <= valor1 - 1; index2 = index2 + 1) {
+    let divs = document.createElement('div')
+    divs.setAttribute('class', 'pixel')
+    container.appendChild(divs)
+  }
 };
 quadro();
+
+// seção 4 - selecionando cor
+const coresAselecionar = document.getElementsByClassName('color');
+const localPalete = document.getElementById('color-palette');
+localPalete.addEventListener('click', function (evento) {
+  for (let i = 0; i <= coresAselecionar.length -1; i = i + 1) {
+    coresAselecionar[i].classList.remove('selected')
+  }
+  evento.target.classList.add('color', 'selected')
+}
+)
